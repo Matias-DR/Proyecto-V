46fce25e2cf2a267bdf192fdf0263170fcfa66c5
Matias-DR <Matias-DR@outlook.com>
2025-02-06 01:04:11 -0300

    sticky filters

--- src/components/post/filter.tsx
+++ src/components/post/filter.tsx
@@ -21,44 +21,46 @@ const Filter = ({ setParams }: Props) => {
   }
 
   return (
-    <article className='flex flex-col gap-2 p-2 bg-blue-300/10 rounded-lg'>
-      <h2 className='text-lg font-bold text-center'>Filtros</h2>
-      <Input
-        placeholder='Nombre/Descripción'
-        debounce={500}
-        onChange={(e) => {
-          setParam('search')(e.target.value)
-        }}
-        className='bg-white/15 text-white font-bold border-blue-400'
-      />
-      <MultiSelect
-        options={CATEGORIES.map((category) => ({ label: category, value: category }))}
-        onValueChange={(value) => setParam('category')(value)}
-        variant='inverted'
-        animation={2}
-        maxCount={3}
-        placeholder='Categoría'
-        className='bg-white/15 text-white font-bold border-blue-400'
-      />
-      <MultiSelect
-        options={CONTINENTS.map((continent) => ({ label: continent, value: continent }))}
-        onValueChange={(value) => setParam('region')(value)}
-        variant='inverted'
-        animation={2}
-        maxCount={3}
-        placeholder='Región'
-        className='bg-white/15 text-white font-bold border-blue-400'
-      />
+    <article className='relative bg-blue-300/10 rounded-lg'>
+      <div className='sticky top-0 flex flex-col gap-2 p-2 '>
+        <h2 className='text-lg font-bold text-center'>Filtros</h2>
+        <Input
+          placeholder='Nombre/Descripción'
+          debounce={500}
+          onChange={(e) => {
+            setParam('search')(e.target.value)
+          }}
+          className='bg-white/15 text-white font-bold border-blue-400'
+        />
+        <MultiSelect
+          options={CATEGORIES.map((category) => ({ label: category, value: category }))}
+          onValueChange={(value) => setParam('category')(value)}
+          variant='inverted'
+          animation={2}
+          maxCount={3}
+          placeholder='Categoría'
+          className='bg-white/15 text-white font-bold border-blue-400'
+        />
+        <MultiSelect
+          options={CONTINENTS.map((continent) => ({ label: continent, value: continent }))}
+          onValueChange={(value) => setParam('region')(value)}
+          variant='inverted'
+          animation={2}
+          maxCount={3}
+          placeholder='Región'
+          className='bg-white/15 text-white font-bold border-blue-400'
+        />
 
-      <MultiSelect
-        options={COUNTRIES.map((country) => ({ label: country, value: country }))}
-        onValueChange={(value) => setParam('country')(value)}
-        variant='inverted'
-        animation={2}
-        maxCount={3}
-        placeholder='País'
-        className='bg-white/15 text-white font-bold border-blue-400'
-      />
+        <MultiSelect
+          options={COUNTRIES.map((country) => ({ label: country, value: country }))}
+          onValueChange={(value) => setParam('country')(value)}
+          variant='inverted'
+          animation={2}
+          maxCount={3}
+          placeholder='País'
+          className='bg-white/15 text-white font-bold border-blue-400'
+        />
+      </div>
     </article>
   )
 }
