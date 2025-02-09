a549289282c4200569a4a8a1c44e93848248c4fd
Matias-DR <Matias-DR@outlook.com>
2025-02-09 01:51:54 -0300

    qwe

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import { Badge } from '@/components/ui/badge'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Post as PostType } from '@/core/post'
 import { FRONTEND_URL } from '@/infra/config'
@@ -15,15 +16,28 @@ const Post = ({ data }: Props) => {
 
   return (
     <div className='size-full p-2 flex flex-col gap-1 border border-blue-300 rounded-lg'>
-      <div className='relative w-full h-48 overflow-hidden border border-blue-300 rounded-md'>
-        <Image
-          src={`${FRONTEND_URL}/api/post/image/${image}`}
-          alt={name}
-          fill
-          className='absolute object-contain'
-        />
-      </div>
-      <h1 className='text-center text-2xl font-bold'>{name}</h1>
+      <Dialog>
+        <DialogTrigger>
+          <div className='relative w-full h-48 overflow-hidden border border-blue-300 rounded-md hover:cursor-pointer'>
+            <Image
+              src={`${FRONTEND_URL}/api/post/image/${image}`}
+              alt={name}
+              fill
+              className='absolute object-contain'
+            />
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
+      <h1 className='text-center text-2xl font-bold line-clamp-1'>{name}</h1>
       <ScrollArea
         vpClassName='p-0'
         className='flex-1'
@@ -32,18 +46,33 @@ const Post = ({ data }: Props) => {
       </ScrollArea>
       <div className='flex items-center gap-2'>
         {category && category.length > 0 && (
-          <Badge className='max-w-[31%]'>
+          <Badge className='relative max-w-[31%]'>
             <span className='truncate'>{category}</span>
+            {category.length > 1 && (
+              <span className='absolute -top-1 -right-1 inline-flex w-auto h-3 px-0.5 pb-[14px] rounded-full bg-blue-500 text-white text-[10px]'>
+                +{category.length}9
+              </span>
+            )}
           </Badge>
         )}
         {region && region.length > 0 && (
-          <Badge className='max-w-[31%]'>
+          <Badge className='relative max-w-[31%]'>
             <span className='truncate'>{region}</span>
+            {region.length > 1 && (
+              <span className='absolute -top-1 -right-1 inline-flex w-auto h-3 px-0.5 pb-[14px] rounded-full bg-blue-500 text-white text-[10px]'>
+                +{region.length}
+              </span>
+            )}
           </Badge>
         )}
         {country && country.length > 0 && (
-          <Badge className='max-w-[31%]'>
+          <Badge className='relative max-w-[31%]'>
             <span className='truncate'>{country}</span>
+            {country.length > 1 && (
+              <span className='absolute -top-1 -right-1 inline-flex w-auto h-3 px-0.5 pb-[14px] rounded-full bg-blue-500 text-white text-[10px]'>
+                +{country.length}
+              </span>
+            )}
           </Badge>
         )}
       </div>
